03abef5b155f5924f941752982e81e399185ba80
Pablo Renero Balgañón <prenero4bc@gmail.com>
2020-03-10 13:33:03 +0100

    Read file by lines

--- ts/openFile.ts
+++ ts/openFile.ts
@@ -1,10 +1,24 @@
 import * as fs from 'fs';
 // Functions
-function fielRead(fileRoute: string): string {
+function fileRead(fileRoute: string): string {
     return fs.readFileSync(FILEDIR + 'test.txt', 'utf8');
+}
+
+function* fileByLines(filefileRoute: string) {
+    const data: string[] = fileRead(FILEDIR + FILE).split('\n').map((a) => a.replace('\r', ''));
+    console.log(data);
+    for (let i of data) {
+        yield i;
+    }
+    return 'done'
 }
 // Code
 const FILEDIR: string = './inputs/';
 const FILE: string = 'test.txts';
-let data : string = fielRead(FILEDIR + FILE);
-console.log(data.split('\n').map((a)=> a.replace('\r', '')));
+
+let input = fileByLines(FILEDIR + FILE);
+let data = input.next();
+while(!data.done){
+    console.log(data.value);
+    data = input.next()
+}
